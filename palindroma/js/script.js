5a8261f9756d1a7de54f7b43942da0e889681cb6
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-08 15:33:12 +0100

    add. verifica parola polindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,21 +4,31 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 
 // 1 chiedere all'utente di inserire una parola
 
-var parola = "ciao";
+var parola = "osso";
 // 2 verificare se parola è Palindroma
 
+var lettere = "";
+var inversa = "";
 
 for (var i = 0; i < parola.length; i++){
 
-console.log(parola[i]);
+  lettere += parola[i];
+  console.log(parola[i]);
 
 }
+console.log(lettere);
 
-for ( var j = parola.length; j >= 0; j--) {
-
+for ( var j = parola.length - 1; j >= 0; j--) {
+  inversa += parola[j];
   console.log(parola[j]);
 }
 
+console.log(inversa);
 
+if ( lettere === inversa ){
 
+  alert("la parola è polindroma");
+} else{
+  alert("la parola non è polindroma");
+}
 // 3 esito verificare
