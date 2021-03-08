affd538e2133beaa8091512b7d2d6da8c5625ff2
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-08 15:12:18 +0100

    add. scheme

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,24 @@
+/*Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
 
+
+// 1 chiedere all'utente di inserire una parola
+
+var parola = "ciao";
+// 2 verificare se parola è Palindroma
+
+
+for (var i = 0; i < parola.length; i++){
+
+console.log(parola[i]);
+
+}
+
+for ( var j = parola.length; j >= 0; j--) {
+
+  console.log(parola[j]);
+}
+
+
+
+// 3 esito verificare
